8f2dae3d143a5c9570c579e23d966ecd8bc46399
CustardCup <daniel.c.chui@gmail.com>
2026-06-17 20:51:34 -0400

    Fix syntax error in changeColour function

--- index1.js
+++ index1.js
@@ -10,5 +10,5 @@ function countWords() {
 function changeColour() {
     const colors = ['lightblue', 'lightgreen', 'lightcoral', 'lightgoldenrodyellow'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    const document.getElementById('myBody').style.backgroundColor = randomColor;
+    document.getElementById('myBody').style.backgroundColor = randomColor;
 }
