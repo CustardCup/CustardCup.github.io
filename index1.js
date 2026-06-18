8743e4c1596f362e073960b5ead0853ffa572f33
CustardCup <daniel.c.chui@gmail.com>
2026-06-17 20:18:22 -0400

    Refactor word count condition for clarity

--- index1.js
+++ index1.js
@@ -2,7 +2,7 @@ let output = document.getElementById("count")
 
 function countWords() {
   const str = document.getElementById("text-input").value;
-  if (str !== "") {
+  if (str) {
   output.textContent = str.trim().split(/\s+/).length;
   } else output.textContent = 0;
 }
