ed000577b42fc1b8c13bbf13ebb74105df41e87e
CustardCup <daniel.c.chui@gmail.com>
2026-06-17 20:36:13 -0400

    Update index1.js

--- index1.js
+++ index1.js
@@ -6,3 +6,9 @@ function countWords() {
   output.textContent = str.trim().split(/\s+/).length;
   } else output.textContent = 0;
 }
+
+function changeColour() {
+    const colors = ['lightblue', 'lightgreen', 'lightcoral', 'lightgoldenrodyellow'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById('myBody').style.backgroundColor = randomColor;
+}
